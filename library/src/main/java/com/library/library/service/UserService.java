16a0c46054e50e0a935d48d0d5cb9ef4bd0be491
mob <1225237693@qq.com>
2020-02-09 20:11:47 +0800

    删除无用代码

--- library/src/main/java/com/library/library/service/UserService.java
+++ library/src/main/java/com/library/library/service/UserService.java
@@ -1,24 +1,14 @@
 package com.library.library.service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.library.library.constant.LibraryConstants;
-import com.library.library.domain.UserCtmDetails;
 import com.library.library.entity.User;
-import com.library.library.entity.Userrole;
 import com.library.library.mapper.UserMapper;
-import com.library.library.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -33,10 +23,6 @@ public class UserService extends ServiceImpl<UserMapper, User> {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @Autowired
-    private JwtUtil jwtUtil;
-    @Autowired
-    private UserroleService userroleService;
 
     /**
      * 用户注册
@@ -50,42 +36,6 @@ public class UserService extends ServiceImpl<UserMapper, User> {
         user.setCreatedate(date);
         user.setUpdatedate(date);
         this.save(user);
-    }
-
-    /**
-     * 验证用户合法性
-     * @param user
-     * @return
-     */
-    public String login(User user) {
-        if (user == null) {
-          throw new RuntimeException("账号或密码错误！");
-        }
-        UserDetails userDetails = this.loadUserByUsernameAndType(user.getUsername(), user.getUsertype());
-        if(passwordEncoder.matches(user.getPassword(), userDetails.getPassword())) {
-            String token = jwtUtil
-                    .createJWT(userDetails.getUsername(), userDetails.getUsername(), LibraryConstants.ROLE_ADMIN);
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            return token;
-        }else {
-            throw new RuntimeException("账号或密码错误！");
-        }
-    }
-
-    public UserDetails loadUserByUsernameAndType(String userName, String userType) {
-        User userLogin = this.lambdaQuery().eq(User::getUsername, userName)
-                .eq(User::getUsertype, userType)
-                .one();
-        if (userLogin == null) {
-            throw new RuntimeException("账号或密码错误！");
-        }
-        List<Userrole> userRoleList = userroleService.listUserRoleByUserId(userLogin.getId());
-        List<SimpleGrantedAuthority> roleNames = userRoleList.stream()
-                .map(ur -> new SimpleGrantedAuthority(ur.getName()))
-                .collect(Collectors.toList());
-        return new UserCtmDetails(userLogin,roleNames);
     }
 
     /**
